ea2101921da1fd695520e9caa2a4590313482d71
Gareth Midwood <gareth@digital-nature.co.uk>
2025-04-25 18:19:20 +0100

    Allow content to be added into option admin pages

--- templates/dn-utilities/admin/option/configure.php
+++ templates/dn-utilities/admin/option/configure.php
@@ -11,17 +11,27 @@ use DigitalNature\Utilities\Config\Setting;
     </div>
 
     <div class="digital-nature-admin-wrap-content">
+        <?php do_action("{$setting->get_option_name()}_configuration_before_panel_title"); ?>
         <div class="digital-nature-admin-wrap-panel-title">
             <?= $setting->get_section_content(); ?>
         </div>
+        <?php do_action("{$setting->get_option_name()}_configuration_after_panel_title"); ?>
+
+        <?php do_action("{$setting->get_option_name()}_configuration_before_panel_body"); ?>
         <div class="digital-nature-admin-wrap-panel-body">
+            <?php do_action("{$setting->get_option_name()}_configuration_panel_body_before_errors"); ?>
             <?php
+            if (!empty( $_GET['settings-updated'] )) {
+                echo '<div class="notice notice-success is-dismissible"><p><strong>Settings saved</strong></p></div>';
+            }
+
             // show error/update messages
             settings_errors($setting->get_messages_slug());
             ?>
+            <?php do_action("{$setting->get_option_name()}_configuration_panel_body_after_errors"); ?>
 
+            <?php do_action("{$setting->get_option_name()}_configuration_panel_body_before_form"); ?>
             <form action="options.php" method="post">
-
                 <?php
                 // output security fields
                 settings_fields($setting->get_option_group());
@@ -31,6 +41,9 @@ use DigitalNature\Utilities\Config\Setting;
                 submit_button( 'Save Settings' );
                 ?>
             </form>
+            <?php do_action("{$setting->get_option_name()}_configuration_panel_body_after_form"); ?>
         </div>
+
+        <?php do_action("{$setting->get_option_name()}_configuration_after_panel_body"); ?>
     </div>
 </div>
